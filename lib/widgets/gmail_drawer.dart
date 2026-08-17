5bb2d5a6390a90ce45a29978dfe9842e61bfc234
akaletekoffilevis <l.akalete20@gmail.com>
2026-08-17 14:49:56 +0100

    feat: ajouter le menu latéral Gmail avec titre rouge et séparateur

---
+++ lib/widgets/gmail_drawer.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class GmailDrawer extends StatelessWidget {
+  const GmailDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Colors.white,
+      shape: const RoundedRectangleBorder(),
+      child: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.fromLTRB(16, 16, 16, 15),
+              child: const Text(
+                'Gmail',
+                style: TextStyle(
+                  color: Color(0xFFEA4335),
+                  fontSize: 22,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Divider(height: 1, color: Colors.grey),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
