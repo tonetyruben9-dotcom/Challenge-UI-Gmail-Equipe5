fbc4db3f73e7645a424c7f012175d716a393f88b
akaletekoffilevis <l.akalete20@gmail.com>
2026-08-19 15:25:41 +0100

    feat: ajouter le libellé Corbeille dans le drawer

--- lib/widgets/gmail_drawer.dart
+++ lib/widgets/gmail_drawer.dart
@@ -188,6 +188,15 @@ class GmailDrawer extends StatelessWidget {
                 style: TextStyle(fontSize: 14, color: Colors.black87),
               ),
             ),
+            ListTile(
+              dense: true,
+              contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+              leading: const Icon(Icons.delete_outline, color: Colors.black87, size: 22),
+              title: const Text(
+                'Corbeille',
+                style: TextStyle(fontSize: 14, color: Colors.black87),
+              ),
+            ),
           ],
         ),
       ),
